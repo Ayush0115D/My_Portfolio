428b5012e552d82fafacf04a44d550f479f45c91
Ayush0115D <dhakreayush578@gmail.com>
2025-06-10 23:27:53 +0530

    added more animations

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -50,8 +50,8 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Adrian, a developer based in Croatia with a passion for
-              code.
+              Hi, I’m Ayush, a developer with a passion for web development and problem solving skills
+              
             </p>
 
             <Button
