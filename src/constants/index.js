6a486511e3aa2d321e3c66d506bb18c27ed392e4
Ayush0115D <dhakreayush578@gmail.com>
2025-06-12 11:59:56 +0530

    some modifications done

---
+++ src/constants/index.js
@@ -0,0 +1,259 @@
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 1, suffix: "+", label: "Years of Experience" },
+  { value: 5, suffix: "+", label: "Completed Projects" },
+  { value: 50, suffix: "+", label: "Questions solved" },
+  { value: 4, suffix: "+", label: "Certifications" },
+
+];
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/HTML5.png"
+  },
+  {
+    imgPath: "/images/logos/CSS3.png",
+  },
+  {
+    imgPath: "/images/logos/git.svg",
+  },
+  {
+    imgPath: "/images/logos/JavaScript.png",
+  },
+  {
+    imgPath: "/images/logos/NPM.png",
+  },
+  {
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    imgPath: "/images/logos/Vite.js.png",
+  },
+  {
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    imgPath: "/images/logos/Tailwind CSS.png",
+  },
+  {
+    imgPath: "/images/logos/Java.png",
+  },
+];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Quality Focus",
+    desc: "Delivering high-quality results while maintaining attention to every detail.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Reliable Communication",
+    desc: "Keeping you updated at every step to ensure transparency and clarity.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Delivery",
+    desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
+  },
+];
+
+const techStackImgs = [
+  {
+    name: "React Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Frontend Developer",
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    name: "Java Developer",
+    imgPath: "/images/logos/java.png",
+  },
+  
+
+];
+
+const techStackIcons = [
+  {
+    name: "Frontend Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Web Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Java Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+];
+
+const expCards = [
+  {
+    review: "Ayush brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    title: "Web Developer",
+    date: "June 2024 - Present",
+    responsibilities: [
+      "Developed and maintained user-facing features for the website.",
+      "Build a interview website with ai voice agent using nodejs",
+      "Optimized web applications for maximum speed and scalability.",
+    ],
+  },
+  {
+    review: "Ayush contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+    imgPath: "/images/exp2.png",
+    logoPath: "/images/logo2.png",
+    title: "JAVA Developer",
+    date: "October 2023 - Present",
+    responsibilities: [
+      "A dedicated coder with strong proficiency in Java and in Data Structures and Algorithms (DSA). I have wide range of knowledge on core data structures such as arrays, linked lists, stacks, queues, trees, and hashmaps. I am well-aware about algorithmic techniques including recursion,backtracking, sorting, searching .",
+      " My Java skills also include understanding of object-oriented programming (OOP), collections framework, and exception handling",
+    ],
+  },
+  {
+    review: "Ayush work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+    imgPath: "/images/exp3.png",
+    logoPath: "/images/logo3.png",
+    title: "Frontend Developer",
+    date: " November 2023- Present",
+    responsibilities: [
+      "I have hands-on experience with core web technologies including HTML5, CSS3, and JavaScript, and I actively work with modern libraries and frameworks such as React.js and Tailwind CSS to develop dynamic and visually appealing applications.",
+      "My skill set includes creating reusable components, managing state, handling APIs, and implementing responsive design for cross-device compatibility. I am familiar with version control using Git & GitHub, and I follow best practices in UI/UX to deliver seamless user experiences",
+
+      "Build a portfolio website with the help of react and use of GSAP for better animations,UI and for optimal performance.Its engine is designed to work more effciently than others. ",
+    ],
+  },
+];
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3", 
+    imgPath: "/images/logo3.png",
+  },
+];
+
+const testimonials = [
+  {
+    name: "Esther Howard",
+    mentions: "@estherhoward",
+    review:
+      "I can’t say enough good things about Ayush. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+    imgPath: "/images/client1.png",
+  },
+  {
+    name: "Wade Warren",
+    mentions: "@wadewarren",
+    review:
+      "Working with Ayush was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+    imgPath: "/images/client3.png",
+  },
+  {
+    name: "Guy Hawkins",
+    mentions: "@guyhawkins",
+    review:
+      "Collaborating with Ayush was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    imgPath: "/images/client2.png",
+  },
+  {
+    name: "Marvin McKinney",
+    mentions: "@marvinmckinney",
+    review:
+      "Ayush was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+    imgPath: "/images/client5.png",
+  },
+  {
+    name: "Floyd Miles",
+    mentions: "@floydmiles",
+    review:
+      "Ayush expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+    imgPath: "/images/client4.png",
+  },
+  {
+    name: "Albert Flores",
+    mentions: "@albertflores",
+    review:
+      "Ayush was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
+    imgPath: "/images/client6.png",
+  },
+];
+
+const socialImgs = [
+  {
+    name: "github",
+    url:"https://github.com/Ayush0115D",
+    imgPath: "/images/github.svg",
+  },
+  {
+    name: "x",
+    imgPath: "/images/x.png",
+  },
+  {
+    name: "linkedin",
+    url:"https://www.linkedin.com/in/ayushdhakre/",
+    imgPath: "/images/linkedin.png",
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  testimonials,
+  socialImgs,
+  techStackIcons,
+  techStackImgs,
+  navLinks,
+};
