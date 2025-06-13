71700721397b66d0456348f8a1827eafe5050c87
Ayush0115D <dhakreayush578@gmail.com>
2025-06-13 23:11:00 +0530

    Rename some files

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,7 @@ import gsap from "gsap";
 import AnimatedCounter from "../components/AnimatedCounter";
 import Button from "../components/Button";
 import { words } from "../constants";
-// import HeroExperience from "../components/models/hero_models/HeroExperience";
-import HeroExperience from "../components/models/hero_models/HeroExperience.jsx";
+import HeroExperience from "../components/models/hero_models/HeroExperience";
 
 const Hero = () => {
   useGSAP(() => {
